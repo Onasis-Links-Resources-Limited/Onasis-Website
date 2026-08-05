1491132846174fd1f0e8ef6ca58c1dab9b3a5f71
zicoly <omojolazion@gmail.com>
2026-08-05 13:37:11 +0100

    Landing Page Version 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider, useTheme } from './context/ThemeContext';
 import Navbar from './components/common/Navbar';
+import Footer from './components/common/Footer'
 import Home from './pages/Home';
 
 import About from './pages/About';
@@ -25,6 +26,7 @@ const AppContent = () => {
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer />
     </div>
   );
 };
